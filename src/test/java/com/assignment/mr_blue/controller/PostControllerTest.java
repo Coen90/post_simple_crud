5648819ad51a5fe3e421e7c617f6128ccb6d66dd
hyuntae <bht9011@gmail.com>
2024-06-20 00:16:31 +0900

    test: add test code for EditPost

--- src/test/java/com/assignment/mr_blue/controller/PostControllerTest.java
+++ src/test/java/com/assignment/mr_blue/controller/PostControllerTest.java
@@ -3,6 +3,7 @@ package com.assignment.mr_blue.controller;
 import com.assignment.mr_blue.domain.Post;
 import com.assignment.mr_blue.repository.PostRepository;
 import com.assignment.mr_blue.request.CreatePostRequest;
+import com.assignment.mr_blue.request.EditPostRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,8 +15,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -79,6 +79,39 @@ class PostControllerTest {
                 .content(json))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1))
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+
+    @Test
+    @DisplayName("Edit Post Test")
+    void testEditPost() throws Exception {
+        // given
+        String title = "test post";
+        String content = "test content";
+        Post post = Post.builder()
+                .title(title)
+                .content(content)
+                .build();
+        Post save = postRepository.save(post);
+
+        // when
+        String editTitle = "edited test post";
+        String editContent = "edited test content";
+        EditPostRequest req = EditPostRequest.builder()
+                .title(editTitle)
+                .content(editContent)
+                .build();
+        String json = objectMapper.writeValueAsString(req);
+
+        // then
+        mockMvc.perform(patch("/api/post/{postId}", save.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(save.getId()))
+                .andExpect(jsonPath("$.title").value(editTitle))
+                .andExpect(jsonPath("$.content").value(editContent))
                 .andDo(MockMvcResultHandlers.print());
 
     }
