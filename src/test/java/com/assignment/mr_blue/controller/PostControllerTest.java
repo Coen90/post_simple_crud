36b4a7403d6b8e4066d04071bc7616bc76362de6
hyuntae <bht9011@gmail.com>
2024-06-20 00:05:54 +0900

    test: add test code for GetPost

--- src/test/java/com/assignment/mr_blue/controller/PostControllerTest.java
+++ src/test/java/com/assignment/mr_blue/controller/PostControllerTest.java
@@ -1,24 +1,51 @@
 package com.assignment.mr_blue.controller;
 
-import com.assignment.mr_blue.request.GetPostListRequest;
+import com.assignment.mr_blue.domain.Post;
+import com.assignment.mr_blue.repository.PostRepository;
+import com.assignment.mr_blue.request.CreatePostRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class PostControllerTest {
 
-    @Test
-    @DisplayName("test")
-    void TestDefault() {
-        GetPostListRequest req1 = GetPostListRequest.builder()
-                .page(1)
-                .size(1000)
-                .build();
-        System.out.println(req1);
-        GetPostListRequest req2 = GetPostListRequest.builder()
-                .build();
-        System.out.println(req2);
-    }
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    @DisplayName("Get Post Test")
+    void testGetPost() throws Exception {
+        //given
+        String title = "test post";
+        String content = "test content";
+        Post post = Post.builder()
+                .title(title)
+                .content(content)
+                .build();
+        Post save = postRepository.save(post);
+
+        //when
+
+        //then
+        mockMvc.perform(get("/api/post/{postId}", save.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(save.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value(title))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content").value(content))
+                .andDo(MockMvcResultHandlers.print());
+    }
 }
--- src/test/java/com/assignment/mr_blue/service/PostServiceTest.java
+++ src/test/java/com/assignment/mr_blue/service/PostServiceTest.java
@@ -3,13 +3,13 @@ package com.assignment.mr_blue.service;
 import com.assignment.mr_blue.domain.Post;
 import com.assignment.mr_blue.repository.PostRepository;
 import com.assignment.mr_blue.request.GetPostListRequest;
+import com.assignment.mr_blue.response.GetPostListResponse;
 import com.assignment.mr_blue.response.GetPostResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 @SpringBootTest
@@ -31,11 +31,11 @@ class PostServiceTest {
                     .build();
             postRepository.save(post);
         }
-        List<GetPostResponse> list = postService.getPostResponseList(GetPostListRequest.builder()
+        GetPostListResponse list = postService.getPostResponseList(GetPostListRequest.builder()
                 .page(0)
                 .size(10)
                 .build());
-        System.out.println(list.stream().map(GetPostResponse::toString)
+        System.out.println(list.getList().stream().map(GetPostResponse::toString)
                 .collect(Collectors.joining("\n")));
     }
 }
